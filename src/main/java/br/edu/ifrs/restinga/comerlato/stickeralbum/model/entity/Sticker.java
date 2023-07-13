f883527fa1f7a2c09c2e2c55b60a7dbcfc0245ac
Eduardo Comerlato <eduardo.comerlato@southsystem.com.br>
2023-07-13 00:17:19 -0300

    Adding validations to Sticker

--- src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/model/entity/Sticker.java
+++ src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/model/entity/Sticker.java
@@ -2,10 +2,8 @@ package br.edu.ifrs.restinga.comerlato.stickeralbum.model.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
@@ -13,7 +11,9 @@ import java.time.LocalDate;
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.EAGER;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.lang.Boolean.FALSE;
 import static java.lang.String.format;
+import static java.util.Objects.isNull;
 
 @Getter
 @Table(name = "sticker")
@@ -65,10 +65,72 @@ public class Sticker {
     private Album album;
 
     public Sticker(Boolean extra, Boolean rare, String version, String name) {
-        this.extra = extra;
-        this.rare = rare;
+        setExtra(extra);
+        setRare(rare);
+        setVersion(version);
+        setName(name);
+    }
+
+    public void setExtra(Boolean extra) {
+        this.extra = (isNull(extra)) ? FALSE : extra;
+    }
+
+    public void setRare(Boolean rare) {
+        this.rare = (isNull(rare)) ? FALSE : rare;
+    }
+
+    public void setVersion(String version) {
+        if (isNull(version) || version.length() > 10) {
+            throw new IllegalArgumentException("Version should have up to 10 characters.");
+        }
         this.version = version;
+    }
+
+    public void setName(String name) {
+        if (isNull(name) || name.length() < 3 || name.length() > 100) {
+            throw new IllegalArgumentException("Name should have between 3 and 100 characters.");
+        }
         this.name = name;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setTeam(String team) {
+        if (team.length() > 3) {
+            throw new IllegalArgumentException("Team should have up to 3 characters.");
+        }
+        this.team = team;
+    }
+
+    public void setPosition(String position) {
+        if (position.length() > 3) {
+            throw new IllegalArgumentException("Position should have up to 3 characters.");
+        }
+        this.position = position;
+    }
+
+    public void setSummoningYear(Integer summoningYear) {
+        if (summoningYear < 1) {
+            throw new IllegalArgumentException("Summoning year must be a positive number.");
+        }
+        this.summoningYear = summoningYear;
+    }
+
+    public void setHeight(Double height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("Height cannot be negative number.");
+        }
+        this.height = height;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
     }
 
     @Override
