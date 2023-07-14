bc571d83ceef275665f8ae4bd9814a4002d10a9d
Eduardo Comerlato <eduardo.comerlato@southsystem.com.br>
2023-07-14 00:37:39 -0300

    EntityFactory impl

---
+++ src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/model/util/EntityFactory.java
@@ -0,0 +1,42 @@
+package br.edu.ifrs.restinga.comerlato.stickeralbum.model.util;
+
+import br.edu.ifrs.restinga.comerlato.stickeralbum.model.entity.Album;
+import br.edu.ifrs.restinga.comerlato.stickeralbum.model.entity.Sticker;
+import com.github.javafaker.Faker;
+
+import java.util.List;
+
+public class EntityFactory {
+
+    private static final Faker faker = new Faker();
+
+    public static Album createAlbum() {
+        try {
+            return new Album(
+                    faker.programmingLanguage().name(),
+                    faker.country().name(),
+                    faker.number().numberBetween(1900, 2023),
+                    List.of(faker.gameOfThrones().city(), faker.lordOfTheRings().location(), faker.elderScrolls().city()),
+                    faker.pokemon().name()
+            );
+        } catch (IllegalArgumentException e) {
+            return createAlbum();
+        }
+    }
+
+    public static Sticker createSticker() {
+        try {
+            var sticker = new Sticker(
+                    faker.random().nextBoolean(),
+                    faker.random().nextBoolean(),
+                    faker.app().version(),
+                    faker.rickAndMorty().character()
+            );
+            sticker.setTeam(faker.rickAndMorty().location().substring(0, 3).toUpperCase());
+            sticker.setPosition(faker.dragonBall().character().substring(0, 3).toUpperCase());
+            return sticker;
+        } catch (IllegalArgumentException e) {
+            return createSticker();
+        }
+    }
+}
--- src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/runner/dao/DAORunner.java
+++ src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/runner/dao/DAORunner.java
@@ -1,5 +1,7 @@
 package br.edu.ifrs.restinga.comerlato.stickeralbum.runner.dao;
 
+import br.edu.ifrs.restinga.comerlato.stickeralbum.model.entity.Album;
+import br.edu.ifrs.restinga.comerlato.stickeralbum.model.entity.Sticker;
 import br.edu.ifrs.restinga.comerlato.stickeralbum.repository.dao.AlbumDAO;
 import br.edu.ifrs.restinga.comerlato.stickeralbum.repository.dao.StickerDAO;
 import lombok.AllArgsConstructor;
@@ -7,6 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+import static br.edu.ifrs.restinga.comerlato.stickeralbum.model.util.EntityFactory.createAlbum;
+import static br.edu.ifrs.restinga.comerlato.stickeralbum.model.util.EntityFactory.createSticker;
 
 @Slf4j
 @Component
@@ -19,8 +26,18 @@ public class DAORunner {
     @Bean
     private CommandLineRunner runDAO() {
         return args -> {
+            final var stickers = new ArrayList<Sticker>();
+            final var albums = new ArrayList<Album>();
 
+            for (int i = 0; i < 20; i++) {
+                stickers.add(createSticker());
+            }
 
+            for (int i = 0; i < 5; i++) {
+                albums.add(createAlbum());
+            }
+
+            log.info("blab");
         };
     }
 }
