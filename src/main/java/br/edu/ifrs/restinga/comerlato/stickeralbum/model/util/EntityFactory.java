d22da1401af943f6e855ba1d8d71189fedbef1cd
Eduardo Comerlato <eduardo.comerlato@southsystem.com.br>
2023-07-15 01:54:03 -0300

    Making factory more interesting

--- src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/model/util/EntityFactory.java
+++ src/main/java/br/edu/ifrs/restinga/comerlato/stickeralbum/model/util/EntityFactory.java
@@ -13,11 +13,11 @@ public class EntityFactory {
     public static Album createAlbum() {
         try {
             var album = new Album(
-                    faker.programmingLanguage().name(),
+                    getName(10, 100),
                     faker.country().name(),
                     faker.number().numberBetween(1900, 2023),
                     List.of(faker.gameOfThrones().city(), faker.lordOfTheRings().location(), faker.elderScrolls().city()),
-                    faker.pokemon().name()
+                    getName(4, 10)
             );
             album.setSpecialStickers(faker.number().numberBetween(0, 5));
             return album;
@@ -32,13 +32,39 @@ public class EntityFactory {
                     faker.random().nextBoolean(),
                     faker.random().nextBoolean(),
                     faker.app().version(),
-                    faker.rickAndMorty().character()
+                    getName(3, 100)
             );
-            sticker.setTeam(faker.rickAndMorty().location().substring(0, 3).toUpperCase());
-            sticker.setPosition(faker.dragonBall().character().substring(0, 3).toUpperCase());
+            sticker.setTeam(getName(1, 3));
+            sticker.setPosition(getName(1, 3));
             return sticker;
         } catch (IllegalArgumentException e) {
             return createSticker();
         }
+    }
+
+    private static String getName(final int min, final int max) {
+        var name = "";
+
+        final var opt = faker.random().nextInt(1, 7);
+
+        switch (opt) {
+            case 1 -> name = faker.lordOfTheRings().character();
+            case 2 -> name = faker.gameOfThrones().character();
+            case 3 -> name = faker.rickAndMorty().character();
+            case 4 -> name = faker.dragonBall().character();
+            case 5 -> name = faker.programmingLanguage().name();
+            case 6 -> name = faker.pokemon().name();
+            default -> name = faker.superhero().name();
+        }
+
+        while (name.length() < min) {
+            name = name.concat(" " + getName(1, max / 2));
+        }
+
+        if (name.length() > max) {
+            name = name.substring(0, max);
+        }
+
+        return name.toUpperCase();
     }
 }
